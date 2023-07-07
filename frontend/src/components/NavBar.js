4d7c94cc932bed54b75fcbccf161f0db1940f290
pmjames3 <pmjames3@ncsu.edu>
2023-07-07 15:12:14 -0400

    Slightly updated NavBar component
    Still need to make it work with BroswerRoute, Routes, and Route

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Link, withRouter} from 'react-router-dom'
+import {BrowserRoute, Routes, Route, Navigate, Link} from 'react-router-dom'
 
 class NavBar extends Component {
     logOut(e) {
@@ -63,6 +63,21 @@ class NavBar extends Component {
             </nav>
         )
     }
+}
+
+function withRouter(Component) {
+    function ComponentWithRouterProp(props) {
+        let location = useLocation();
+        let navigate = useNavigate();
+        let params = useParams();
+        return (
+            <Component
+                {...props}
+                router={{ location, navigate, params }}
+            />
+        );
+    }
+    return ComponentWithRouterProp;
 }
 
 export default withRouter(NavBar)
